aa8b1f6c7f81eeaec35482621c1cb4495735945e
KavinPrasad2948 <kavinprasad2948@gmail.com>
2024-05-20 12:53:31 +0530

    :star::star::star::star:

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const parser = require("body-parser");
+// const { JSONFilePreset } = require("lowdb/node");
 const { createFile, readDirectory } = require("./Utils/CreateFile");
-const mongoose = require('./DB/DB');
 const HTTP_SERVER = express();
 const PORT = '5000';
 
 HTTP_SERVER.use(parser.json());
 
-HTTP_SERVER.listen(PORT,()=>{
-    console.log("Server is running on port 5000");
-})
+HTTP_SERVER.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 
 HTTP_SERVER.post("/create", (request, response) => {
     const { fileName, fileContent } = request.body;
@@ -26,3 +27,14 @@ HTTP_SERVER.post("/create", (request, response) => {
         });
       });
   });
+  HTTP_SERVER.get("/files", (request, response) => {
+    readDirectory()
+    .then(files =>{
+      response.status(200).json(files);
+    })
+    .catch((error) => {
+      response.status(500).json({
+        message: "Error reading files",error,
+      })
+    });
+  })
